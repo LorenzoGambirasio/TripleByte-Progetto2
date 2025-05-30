1d7ff52b1ee76299fe18c4334117fbc33093dac4
LorenzoGambirasio <126600550+LorenzoGambirasio@users.noreply.github.com>
2025-05-30 10:03:27 +0200

    inizio vista cittadini

--- servizio_sanitario/main/views.py
+++ servizio_sanitario/main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Cittadino
 
 def dashboard(request):
     return render(request, 'home.html')
 
+
 def lista_cittadini(request):
     return render(request, 'cittadini.html')
+
 
 def lista_ospedali(request):
     return render(request, 'ospedali.html')
